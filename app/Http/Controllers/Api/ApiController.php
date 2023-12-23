230290275503390f5b2c49573ee6f1d08a545867
Vladimir547 <germes547@gmail.com>
2023-12-23 18:57:14 +0300

    change register user with service

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Service\NewUser;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\Role;
@@ -11,26 +12,8 @@ use Illuminate\Support\Facades\Hash;
 use DB;
 class ApiController extends Controller
 {
-    public function registration (Request $request) {
-        $request->validate([
-           'first_name' => 'required|min:2',
-            'last_name' => 'required|min:2',
-            'phone'=> 'required||regex:/^(\+?\d+)$/|min:8',
-            'email'=> 'required|email|unique:users',
-            'password'=> 'required|confirmed|min:6',
-        ]);
-        User::create([
-            'first_name' => $request->first_name,
-            'last_name' => $request->last_name,
-            'phone' => $request->phone,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-            'role_id' => 2
-        ]);
-        return response()->json([
-            "status" => true,
-            "message" => "User created"
-        ]);
+    public function registration (Request $request,  NewUser $new) {
+         return $new -> addUser($request);
     }
     public function login(Request $request){
 
